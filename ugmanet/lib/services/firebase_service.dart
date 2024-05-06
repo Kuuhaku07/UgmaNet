84c770cfd67699bb80be1d2934e651d038f7ad62
Andryus27 <andryusguaiquirima@gmail.com>
2024-05-05 23:41:16 -0400

    que ladilla esto xd

--- ugmanet/lib/services/firebase_service.dart
+++ ugmanet/lib/services/firebase_service.dart
@@ -2,6 +2,7 @@ import "package:cloud_firestore/cloud_firestore.dart";
 import 'package:UgmaNet/visual/Screens/feed.dart';
 //hablame, se ve?
 //segundo intento
+//tercero y ultimo
 FirebaseFirestore db = FirebaseFirestore.instance;
 
 Future<List> getUsuarios() async {
